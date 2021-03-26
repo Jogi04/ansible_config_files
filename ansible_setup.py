1b86f680d681a6c6f21ac91ca23723bc8dbf1f25
Jogi123 <testmail@test.com>
2021-03-26 15:05:52 +0100

    updtated ansible_setup.py

--- ansible_setup.py
+++ ansible_setup.py
@@ -1,23 +1,44 @@
-#/bin/python
+#!/bin/python
+
 
 import os
 import sys
 import subprocess
+import re
 from termcolor import cprint
 
 
 class AnsibleSetup:
-    def __init__(self, username, server_ip):
+    def __init__(self, username, server_ips, initial_setup=False):
         self.username = username
-        self.server_ip = server_ip
-        self.package_manager = self.check_underlying_package_manager()
-        self.install_ansible()
-        self.install_openssh_client()
-        self.start_and_enable_sshd()
-       	self.generate_ssh_keys()
-        self.copy_public_key_to_server()
+        self.server_ips = server_ips
+        self.initial_setup = initial_setup
+
+    def setup(self):
+        """
+        main function; if initial_setup == False, only a key will be copied; otherwise it will install ansible and ssh,
+        start and enable ssh, generate ssh keys
+        """
+        if self.initial_setup:
+            system_package_manager = self.check_underlying_package_manager()
+            self.install_ansible(system_package_manager)
+            self.install_openssh_client(system_package_manager)
+            self.start_and_enable_sshd()
+            self.generate_ssh_keys()
+
+        # get list of IPs from inventory file and copy keys to each defined server
+        with open(self.server_ips, 'r') as f:
+            for line in f:
+                ip = re.match(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', line)
+                if ip:
+                    # self.copy_public_key_to_server(ip)
+                    print(ip.group())
 
     def check_underlying_package_manager(self):
+        """
+        checks the underlying package manager to install required software later on
+        :return: package manager if found
+        """
         check_package_manager = subprocess.call(['which', 'apt'])
         if check_package_manager == 0:
             package_manager = 'apt'
@@ -40,32 +61,51 @@ class AnsibleSetup:
         print('Exiting...')
         sys.exit(1)
 
-    def install_ansible(self):
-        if self.package_manager == 'apt':
+    def install_ansible(self, package_manager):
+        """
+        install ansible from the official repositories
+        :param package_manager: which underlying package manager to use
+        """
+        if package_manager == 'apt':
             os.system('sudo -S apt install ansible')
-        elif self.package_manager == 'pacman':
+        elif package_manager == 'pacman':
             os.system('sudo -S pacman -S ansible')
-        elif self.package_manager == 'yum':
+        elif package_manager == 'yum':
             os.system('sudo -S yum install ansible')
 
-    def install_openssh_client(self):
-        if self.package_manager == 'apt':
+    def install_openssh_client(self, package_manager):
+        """
+        install openssh client from the official repositories
+        :param package_manager: which underlying package manager to use
+        """
+        if package_manager == 'apt':
             os.system('sudo -S apt install openssh-client')
-        elif self.package_manager == 'pacman':
+        elif package_manager == 'pacman':
             os.system('sudo -S pacman -S openssh')
-        elif self.package_manager == 'yum':
+        elif package_manager == 'yum':
             os.system('sudo -S yum install openssh-clients')
 
     def start_and_enable_sshd(self):
+        """
+        start and enable ssh service via systemd
+        """
         os.system('sudo systemctl enable --now sshd')
 
     def generate_ssh_keys(self):
+        """
+        generate ssh keys which ansible requires
+        """
         os.system('ssh-keygen')
 
-    def copy_public_key_to_server(self):
-        key_location = str(input('Enter the location of the newly created ssh key [~/.ssh/<key_name>.pub]: '))
-        os.system(f'ssh-copy-id -i {key_location} {self.username}@{self.server_ip}')
+    def copy_public_key_to_server(self, server_ip):
+        """
+        copy an existing ssh public key to a server
+        :param server_ip: ip of the server the keys are supposed to be copied onto
+        """
+        key_location = str(input('Enter the location of the ssh key [~/.ssh/<key_name>.pub]: '))
+        os.system(f'ssh-copy-id -i {key_location} {self.username}@{server_ip}')
 
 
 if __name__ == '__main__':
-    test = AnsibleSetup('<username>', '<server_ip>')
+    setup = AnsibleSetup('<username>', 'inventory', False)
+    setup.setup()
