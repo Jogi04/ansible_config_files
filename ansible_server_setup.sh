fc527520f76721740204d7a063e94f119ad37f7b
Jogi123 <testmail@test.com>
2021-09-11 19:33:12 +0200

    updated ansible_server_setup.sh

--- ansible_server_setup.sh
+++ ansible_server_setup.sh
@@ -5,4 +5,5 @@ passwd ansible
 touch /etc/sudoers.d/ansible
 echo 'ansible ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/ansible
 
+mkdir /home/ansible/.ssh
 sftp jogi@10.0.0.100:/home/jogi/.ssh/ansible.pub /home/ansible/.ssh/ansible.pub
