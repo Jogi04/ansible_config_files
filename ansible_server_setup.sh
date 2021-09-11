0979f946e919449e28400426b160f0185a4db93d
Jogi123 <testmail@test.com>
2021-09-11 20:19:03 +0200

    updated ansible_server_setup.sh

--- ansible_server_setup.sh
+++ ansible_server_setup.sh
@@ -8,7 +8,10 @@ echo 'ansible ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/ansible
 apt install openssh-server -y
 systemctl start sshd
 systemctl enable sshd
+
 mkdir /home/ansible/.ssh
-sftp jogi@10.0.0.100:/home/jogi/.ssh/ansible.pub /home/ansible/.ssh/ansible.pub
-chmod 644 /home/ansible/.ssh/ansible.pub
-chown ansible:ansible /home/ansible/.ssh/ansible.pub
+chmod 744 -R /home/ansible/.ssh/
+chown -R ansible:ansible /home/ansible/.ssh/
+sftp jogi@10.0.0.100:/home/jogi/.ssh/ansible.pub /home/jogi/ansible.pub
+cat  /home/jogi/ansible.pub >> /home/ansible/.ssh/.authorized_keys
+rm /home/jogi/ansible.pub
