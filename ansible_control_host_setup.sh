4d00f08610edb777b247ee8a157d72e646c4d449
Jogi123 <testmail@test.com>
2021-09-25 09:54:00 +0200

    updated ansible_control_host_setup.sh

--- ansible_control_host_setup.sh
+++ ansible_control_host_setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+
+useradd -m -s /bin/bash ansible
+passwd ansible
+touch /etc/sudoers.d/ansible
+echo 'ansible ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/ansible
+
 apt install ansible
 apt install git
-git clone https://github.com/Jogi123/ansible_config_files.git  ~
+git clone https://github.com/Jogi123/ansible_config_files.git  /home/ansible/ansible_config_files
