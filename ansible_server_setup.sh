2f8f9e1628487d1ffd72e503fc5accdbb39dbc67
Jogi123 <testmail@test.com>
2021-10-03 10:26:32 +0200

    added public key directly

--- ansible_server_setup.sh
+++ ansible_server_setup.sh
@@ -11,6 +11,8 @@ systemctl start sshd
 systemctl enable sshd
 
 mkdir /home/ansible/.ssh
-ssh ansible@10.0.0.100 "cat /home/ansible/.ssh/ansible.pub" >> /home/ansible/.ssh/authorized_keys
+cat << EOF >> /home/ansible/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCpGmT8qDe8h2how2LQ3hRiqXRVQVgsyhkfFRuL6EOW0kMQmVBdP0erbom01hls21VKWwQquqUAiZqve+BYT/ov1k070A7gaGKQdDG8avZzSC1az0SQZOmpA7jl3hceO8kFQJSA2M54c91lb+DkUsNFpUokp6w1wEAUW1Gh28b1xxWbRpvv9Dd66nwI8bv8uNMGGSgJy+mUJpnNHEP5efDLIeJnz2OeTUbnrJmDQb3jUG+dfT2KVq8MTfuf4AzX8W8Fkx0Q8AJsLEF/Cwgv5I1px2kTpbdJrANMcSxK3dLIzri6f5IS8/VCaLpMzFZL/PtvkPotoyKZbdlKUyDDpdRNRpRVpjiexFRkD2sLS0nHUwSM9fQln2v9PLcj7XhdtXDf7WH327LXoCp6atX06/OiUj5NabgX7Xww6BwvRm1lFqvxYoSRW8uJEonWDYyWjJfv5oU5FtX0cAAxEp3mqsDsR2/+MbhGpzqfkLPV8XdptW89sax4rMrtMW01Rg2+OB8= ansible@ansible
+EOF
 chmod 744 -R /home/ansible/.ssh/
 chown -R ansible:ansible /home/ansible/.ssh/
